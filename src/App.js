236246cdbf8d9347139e27c36e6c86bc17bd3118
Kishan Mehta <kishan@mobifly.co.uk>
2019-02-14 17:10:54 +0530

    Add reformatting.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import './App.css'
 import SearchBook from './SearchBook';
 import ListBooks from './ListBooks';
 
 class BooksApp extends React.Component {
-  state = {
-    books : []
-  }
+    state = {
+        books: []
+    }
 
-  handleStatusChange = (book, shelf) => {
-    const books = this.state.books.filter((e) => e.id !== book.id)
-    book.shelf = shelf
-    BooksAPI.update(book, shelf)
-    .then(() => {
-      this.setState((currState) => ({
-        books : books.concat(book)
-      }))
-    })
-  }
+    handleStatusChange = (book, shelf) => {
+        const books = this.state.books.filter((e) => e.id !== book.id)
+        book.shelf = shelf
+        BooksAPI.update(book, shelf)
+            .then(() => {
+                this.setState((currState) => ({
+                    books: books.concat(book)
+                }))
+            })
+    }
   
-  componentDidMount () {
-    BooksAPI.getAll()
-    .then((books) => {
-        this.setState(() => ({
-            books
-        }))
-    })
-    
-  }
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
+                this.setState(() => ({
+                    books
+                }))
+            })
 
-  render() {
-    return (
-      <div className="app">
-        <Route path="/search" render={() => (
-          <SearchBook
-            books={this.state.books} 
-            onStatusChange={(book, shelf) => {this.handleStatusChange(book, shelf)}}  
-          />
-        )} />
-        <Route exact path="/" render={() => (
-            <ListBooks
-              books={this.state.books}
-              onStatusChange={(book, shelf) => {this.handleStatusChange(book, shelf)}}
-            />
-        )} />
-    
-      </div>
-    )
-  }
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Route path="/search" render={() => (
+                    <SearchBook
+                        books={this.state.books}
+                        onStatusChange={(book, shelf) => {
+                            this.handleStatusChange(book, shelf)
+                        }}
+                    />
+                )}/>
+                <Route exact path="/" render={() => (
+                    <ListBooks
+                        books={this.state.books}
+                        onStatusChange={(book, shelf) => {
+                            this.handleStatusChange(book, shelf)
+                        }}
+                    />
+                )}/>
+
+            </div>
+        )
+    }
 }
 
 export default BooksApp;
